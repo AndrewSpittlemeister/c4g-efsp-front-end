dd570d863dda9ef2182e0ee68fcedd876d7fba1b
Andrew Spittlemeister <andrew.spittlemeister@gmail.com>
2023-02-16 22:14:28 -0600

    don't use relative imports I guess

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import NavItem from "./NavItem";
+import NavItem from "@/components/NavItem";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
