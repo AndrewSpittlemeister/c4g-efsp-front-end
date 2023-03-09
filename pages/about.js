49d619e68404c445486f6ecb33b3c156dbee1d8d
Andrew Spittlemeister <andrew.spittlemeister@gmail.com>
2023-03-08 19:19:01 -0600

    added team contributions

--- pages/about.js
+++ pages/about.js
@@ -57,6 +57,7 @@ export default function AboutPage() {
               <th>Name</th>
               <th>Role</th>
               <th>Goal</th>
+              <th>Contributions</th>
             </tr>
           </thead>
           <tbody>
@@ -64,21 +65,33 @@ export default function AboutPage() {
               <td>{'Andrew Spittlemeister'}</td>
               <td>{'Backend/Data Engineer and Team lead'}</td>
               <td>{'Contribute to building an application that can streamline the process of managing shelter funding requests, checking for duplication, and providing an informative view for administrators. I would also like to help provide support to my team and facilitate their abilities to create a well-rounded application.'}</td>
+              <td>
+                Interfaced with customer and created presentations & evaluations for this project. Also pulled together the Vercel deployment, designed application navigation, enabled PWA support, and contributed to CSS formatting.
+              </td>
             </tr>
             <tr>
               <td>{'Bryan Xian '}</td>
               <td>{'Backend/Data Engineer'}</td>
               <td>{'Develop a working and easy to use MVP for client. I would like to make a positive and lasting impact on the Metro Atlanta community by the end of this project. I plan to do this by using my software engineering skills to save employees of United Way of Atlanta time, so they can better serve the community.'}</td>
+              <td>
+                Aggregated user needs into application logic & designed database schemas such that it could create a scalable architecture to include user identities and relationships between current and future metadata.
+              </td>
             </tr>
             <tr>
               <td>{'Marium Ali'}</td>
               <td>{'Full Stack Engineer'}</td>
               <td>{"Develop a working and easy to use MVP for client to be able to use. I would like the client to be able to process a new request for shelter as soon as possible identifying the duplication rules and make a decision without much manual interaction. Additionally, the interface should be easy to use and training-free. My goal is to use my expertise and make that happen as a team!"}</td>
+              <td>
+                Designed the project about page (team web page) and form submission page, added Google authentication functionality, and contributed to defining funding application logic.
+              </td>
             </tr>
             <tr>
               <td>{'Pallavi Bhatnagar'}</td>
               <td>{'Full Stack Engineer'}</td>
               <td>{'Develop a working and easy to use Minimal Loveable Product for the client. Working with the team I will contibute to all phases of software devlopment lifecycle for United Way. My goal will be to ensure we maintain high quality work and meet all the requirements stated by the partner.'}</td>
+              <td>
+                Interfaced with customer and DreamHost to allocate a hosted database with non-profit status and hooked it up to our hosted application. Implemented the database schemas inside this database and created API calls to access the database from front-end application logic.
+              </td>
             </tr>
           </tbody>
         </table>
