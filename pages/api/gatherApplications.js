17471c921be981f468a09cd98670b8781b82d8e2
Andrew Spittlemeister <andrew.spittlemeister@gmail.com>
2023-03-22 21:55:03 -0500

    updated confirmation page to query database (WIP)

--- pages/api/gatherApplications.js
+++ pages/api/gatherApplications.js
@@ -2,10 +2,18 @@ import executeQuery from "@/lib/db";
 
 
 export default async function handler(req, res) {
+    var query_string = "select * from Application";
+    var query_values = [];
+
+    if (req.query.hasOwnProperty('identity')) {
+        query_string = query_string.concat(" WHERE ApplicantId = ?");
+        query_values.push(req.query.identity);
+    }
     try {
         const db_result = await executeQuery(
             {
-                query: 'select * from Application'
+                query: query_string,
+                values: query_values
             }
         );
         res.status(200).json({ result: db_result})
--- pages/confirmation.js
+++ pages/confirmation.js
@@ -7,10 +7,89 @@ export default function ConfirmationPage({ params }) {
     const router = useRouter();
     const { data: session } = useSession();
 
-    console.log(`params: ${router.query}`)
     const data = router.query;
+    console.log("confirmation parameters");
+    console.log(data);
+
+    // will be used to populate the similar records HTML data below
+    var similar_records = [];
+
+    console.log("unpopulated similar records:");
+    console.log(similar_records);
 
     // TODO: gather similar records from the database
+    fetch(
+        "/api/gatherApplicants",
+        {
+            method: "GET",
+            headers: {
+                "accept": "application/json",
+            },
+        },
+    ).then(
+        (applicant_res) => applicant_res.json()
+    ).then(
+        (applicant_json) => {
+            console.log("similar applicants:");
+            for (const applicant of applicant_json.result) {
+                console.log(`Applicant:`);
+                console.log(applicant);
+
+                const app_dob = applicant.DOB.split("T")[0];
+                var similar = false;
+
+                // will list if the date of birth or last name is the same
+                if (app_dob === data.dob)  {
+                    similar = true;
+                } else if (data.lastname == applicant.LastName) {
+                    similar = true;
+                }
+
+                if (similar) {
+                    // gather the application data for this person
+                    var record = {
+                        name: `${applicant.FirstName} ${applicant.MiddleName} ${applicant.LastName}`,
+                        dob: app_dob,
+                        history: []
+                    };
+                    
+                    // use gatherApplications route with the applicant ID to do so
+                    fetch(
+                        `/api/gatherApplications?identity=${applicant.ApplicantId}`,
+                        {
+                            method: "GET",
+                            headers: {
+                                "accept": "application/json",
+                            },
+                        },
+                    ).then(
+                        (application_res) => application_res.json()
+                    ).then(
+                        (application_json) => {
+                            console.log(`Application History for ${applicant.FirstName} ${applicant.MiddleName} ${applicant.LastName}`);
+                            for (const application in application_json.result) {
+                                console.log(`${applicant}`);
+                                record.history.push(
+                                    {
+                                        "date": application.ApprovalDate,
+                                        "funding_phase": application.FundingPhase,
+                                        "jurisdiction": application.Jurisdiction,
+                                        "vendor": application.FundingVendor,
+                                    }
+                                );
+                            }
+                        }
+                    );
+
+                    // append to similar_records
+                    similar_records.push(record);
+                }
+            }
+        }
+    );
+
+    console.log("populated similar records:");
+    console.log(similar_records);
 
     // TODO: add callback for approve and deny buttons to either send data to database or kick back to form page.
 
