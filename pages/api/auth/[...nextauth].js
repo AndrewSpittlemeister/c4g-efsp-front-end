a200c0e20fa067b9b9bddf4358cf34bc4396b968
Marium Ali <mali328@gatech.edu>
2023-03-25 20:25:39 +0000

    updated credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,28 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "844640573338-207k3efn3rh7elqfrhes59gndjah6akr.apps.googleusercontent.com",
-            clientSecret: "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",
-            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+            clientId: "844640573338-sapsnsiuocmal1gll5a4ds4sel6rlv4f.apps.googleusercontent.com",
+            clientSecret: "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",
         })
-    ],
-    jwt: {
-        encryption: true
-    },
-    secret: "secret token",
-    //Callback here
-    callbacks: {
-        async jwt(token, account) {
-            if (account?.accessToken) {
-                token.accessToken = account.accessToken
-            }
-            return token;
-        },
-        redirect: async (url, _baseUrl) => {
-            if (url === '/user') {
-                return Promise.resolve('/')
-            }
-            return Promise.resolve('/')
-        }
-    }
+    ]
 });
