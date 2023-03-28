6e4938f8d4cb3f0697d1f9f526e5224226a222a4
Andrew Spittlemeister <spittlemeister@brsc.com>
2023-03-28 13:03:54 -0500

    resolving merge conflicts

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,10 +6,7 @@ export default NextAuth({
         GoogleProvider({
             clientId: "844640573338-sapsnsiuocmal1gll5a4ds4sel6rlv4f.apps.googleusercontent.com",
             clientSecret: "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",
-<<<<<<< HEAD
             authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
-=======
->>>>>>> origin/main
         })
     ]
 });
