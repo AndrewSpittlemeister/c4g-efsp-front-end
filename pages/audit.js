ce8777f53c42ca7b67a989941e3bcb4b535025ea
Marium Ali <mali328@gatech.edu>
2023-04-07 13:22:46 +0100

    Audit Table with filtering, sorting added

--- pages/audit.js
+++ pages/audit.js
@@ -1,13 +1,345 @@
 import styles from '@/styles/Home.module.css'
-import React, { useEffect, useState } from "react"
+import styled from 'styled-components'
+import React, { useEffect, useState, useMemo } from "react"
 import { useSession } from "next-auth/react"
 import { useRouter } from 'next/router';
 import getUserRole from "@/lib/users";
+import { useTable, useFilters, useGlobalFilter, useAsyncDebounce, useSortBy } from 'react-table'
 
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+  `
+
+function SliderColumnFilter({
+    column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+    // Calculate the min and max
+    // using the preFilteredRows
+
+    const [min, max] = useMemo(() => {
+        let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+        let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+        preFilteredRows.forEach(row => {
+            min = Math.min(row.values[id], min)
+            max = Math.max(row.values[id], max)
+        })
+        return [min, max]
+    }, [id, preFilteredRows])
+
+    return (
+        <>
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={filterValue || min}
+                onChange={e => {
+                    setFilter(parseInt(e.target.value, 10))
+                }}
+            />
+            <button onClick={() => setFilter(undefined)}>Off</button>
+        </>
+    )
+}
+
+
+function SelectColumnFilter({
+    column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+    // Calculate the options for filtering
+    // using the preFilteredRows
+    const options = React.useMemo(() => {
+        const options = new Set()
+        preFilteredRows.forEach(row => {
+            options.add(row.values[id])
+        })
+        return [...options.values()]
+    }, [id, preFilteredRows])
+
+    // Render a multi-select box
+    return (
+        <select
+            value={filterValue}
+            onChange={e => {
+                setFilter(e.target.value || undefined)
+            }}
+        >
+            <option value="">All</option>
+            {options.map((option, i) => (
+                <option key={i} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+    )
+}
+
+function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+}) {
+    const count = preFilteredRows.length
+
+    return (
+        <input
+            value={filterValue || ''}
+            onChange={e => {
+                setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+            }}
+            placeholder={`Search ${count} records...`}
+        />
+    )
+}
+
+function GlobalFilter({
+    preGlobalFilteredRows,
+    globalFilter,
+    setGlobalFilter,
+}) {
+    const count = preGlobalFilteredRows.length
+    const [value, setValue] = React.useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+        setGlobalFilter(value || undefined)
+    }, 200)
+
+    return (
+        <span>
+            Search:{' '}
+            <input
+                value={value || ""}
+                onChange={e => {
+                    setValue(e.target.value);
+                    onChange(e.target.value);
+                }}
+                placeholder={`${count} records...`}
+                style={{
+                    fontSize: '1.1rem',
+                    border: '0',
+                }}
+            />
+        </span>
+    )
+}
+
+function Table({ columns, data }) {
+
+    const defaultColumn = React.useMemo(
+        () => ({
+            // Let's set up our default Filter UI
+            Filter: DefaultColumnFilter,
+        }),
+        []
+    )
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        state,
+        visibleColumns,
+        preGlobalFilteredRows,
+        setGlobalFilter,
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn, // Be sure to pass the defaultColumn option
+        },
+        useFilters, // useFilters!
+        useGlobalFilter, // useGlobalFilter!
+        useSortBy,
+    )
+
+    // We don't want to render all 2000 rows for this example, so cap
+    // it at 20 for this use case
+    const firstPageRows = rows.slice(0, 20)
+
+    return (
+        <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                // Add the sorting props to control sorting. For this example
+                                // we can add them into the header props
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    {/* Add a sort direction indicator */}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? ' 🔽'
+                                                : ' 🔼'
+                                            : ''}
+                                    </span>
+
+
+                                </th>
+
+                            ))}
+                        </tr>
+                    ))}
+
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>
+                                    {/* Render the columns filter UI */}
+                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                    <tr>
+                        <th
+                            colSpan={visibleColumns.length}
+                            style={{
+                                textAlign: 'left',
+                            }}
+                        >
+                            <GlobalFilter
+                                preGlobalFilteredRows={preGlobalFilteredRows}
+                                globalFilter={state.globalFilter}
+                                setGlobalFilter={setGlobalFilter}
+                            />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {firstPageRows.map(
+                        (row, i) => {
+                            prepareRow(row);
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell => {
+                                        return (
+                                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        }
+                    )}
+                </tbody>
+            </table>
+            <br />
+            <div>Showing the first {rows.length <= 20 ? rows.length : 20} results of {rows.length} entries</div>
+            <div>
+                <pre>
+                    <code>{console.log(JSON.stringify(state.filters, null, 2))}</code>
+                </pre>
+            </div>
+        </>
+    )
+}
+
+function filterGreaterThan(rows, id, filterValue) {
+    return rows.filter(row => {
+        const rowValue = row.values[id]
+        return rowValue >= filterValue
+    })
+}
+
+// This is an autoRemove method on the filter function that
+// when given the new filter value and returns true, the filter
+// will be automatically removed. Normally this is just an undefined
+// check, but here, we want to remove the filter if it's not a number
+filterGreaterThan.autoRemove = val => typeof val !== 'number'
 
 export default function ConfirmationPage({ params }) {
     const router = useRouter();
     const { data: session, status } = useSession();
+    const [allRecords, setAllRecords] = useState([]);
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'ID',
+                columns: [
+                    {
+                        Header: 'Applicant ID',
+                        accessor: 'ApplicantId',
+                        Filter: SliderColumnFilter,
+                        filter: filterGreaterThan,
+                    },
+                ],
+            },
+            {
+                Header: 'Name',
+                columns: [
+                    {
+                        Header: 'First Name',
+                        accessor: 'FirstName',
+                    },
+                    {
+                        Header: 'Middle Name',
+                        accessor: 'MiddleName',
+                    },
+                    {
+                        Header: 'Last Name',
+                        accessor: 'LastName',
+                    },
+                ],
+            },
+            {
+                Header: 'Additional Information',
+                columns: [
+                    {
+                        Header: 'Date Of Birth',
+                        accessor: 'DOB',
+                    },
+                    {
+                        Header: 'City',
+                        accessor: 'City',
+                        Filter: SelectColumnFilter,
+                        filter: 'includes',
+                    },
+                    {
+                        Header: 'Country',
+                        accessor: 'Country',
+                        Filter: SelectColumnFilter,
+                        filter: 'includes',
+                    },
+                    {
+                        Header: 'Postal Code',
+                        accessor: 'PostalCode',
+                    },
+                    {
+                        Header: 'Street Address',
+                        accessor: 'StreetAddress',
+                    },
+                ],
+            },
+        ],
+        []
+    )
 
     useEffect(
         () => {
@@ -15,9 +347,37 @@ export default function ConfirmationPage({ params }) {
                 console.log(`status is: ${status}`);
             }
             onStatusChange();
-        }, 
+        },
         [status]
     )
+
+    useEffect(
+        () => {
+            async function getAllRecords() {
+                if (router.isReady) {
+
+                    let records_res = await fetch(
+                        `/api/gatherApplicants`,
+                        {
+                            method: "GET",
+                            headers: {
+                                "accept": "application/json",
+                            },
+                        },
+                    );
+                    let records = await records_res.json();
+
+                    console.log("Setting All Applicant Records");
+                    console.log(records);
+                    setAllRecords(records.result);
+                } else {
+                    setAllRecords([]);
+                }
+            }
+            getAllRecords();
+        },
+        [router.isReady]
+    );
 
     if (status != "authenticated") {
         return (
@@ -27,7 +387,7 @@ export default function ConfirmationPage({ params }) {
                 <div className={styles.card}>
                     <p>Navigate to the home page and sign-in first.</p>
                     <br></br>
-                    <button className={styles.button} style={{marginLeft: 'auto', marginRight: 'auto', marginTop: "10px", marginBottom: "5px"}} onClick={() => router.push('/')}>
+                    <button className={styles.button} style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: "10px", marginBottom: "5px" }} onClick={() => router.push('/')}>
                         Return Home
                     </button>
                 </div>
@@ -42,7 +402,7 @@ export default function ConfirmationPage({ params }) {
                     <div className={styles.card}>
                         <p>This page requires admin-level privileges to access, sign in with a different account with these privileges to use this page.</p>
                         <br></br>
-                        <button className={styles.button} style={{marginLeft: 'auto', marginRight: 'auto', marginTop: "10px", marginBottom: "5px"}} onClick={() => router.push('/')}>
+                        <button className={styles.button} style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: "10px", marginBottom: "5px" }} onClick={() => router.push('/')}>
                             Return Home
                         </button>
                     </div>
@@ -52,6 +412,9 @@ export default function ConfirmationPage({ params }) {
             return (
                 <main className={styles.main}>
                     <h1>Audit Records</h1>
+                    <Styles>
+                        <Table columns={columns} data={allRecords} />
+                    </Styles>
                 </main>
             );
         }
